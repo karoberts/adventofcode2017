28047538f57251ab822099d464b2767848a81efb
karoberts <cppwriter@yahoo.com>
2019-01-14 11:41:54 -0800

    solved 8-2

--- 08.py
+++ 08.py
@@ -6,6 +6,7 @@ pat = re.compile(r'^([a-z]+) (inc|dec) ([\-\d]+) if ([a-z]+) (<|>|==|!=|<=|>=) (
 
 regs = defaultdict(lambda:0)
 
+max_val = -9999999999
 with open('08.txt') as f:
     for line in (l.strip() for l in f):
         m = pat.match(line)
@@ -23,7 +24,10 @@ with open('08.txt') as f:
             delt = int(m.group(3))
             delt *= -1 if m.group(2) == 'dec' else 1
             regs[m.group(1)] += delt
+            if regs[m.group(1)] > max_val:
+                max_val = regs[m.group(1)]
 
 max_reg = max(regs, key=lambda x:regs[x])
-print(max_reg, regs[max_reg])
+print('part1', max_reg, regs[max_reg])
+print('part2', max_val)
         
