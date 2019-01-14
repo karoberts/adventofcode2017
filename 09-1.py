4ffd559ac5f8684a6ba080046507cb169976d75d
karoberts <cppwriter@yahoo.com>
2019-01-14 12:59:09 -0800

    solved 9

---
+++ 09-1.py
@@ -0,0 +1,29 @@
+
+with open('09.txt') as f:
+    l = f.readline().strip()
+
+    groups = []
+    score = 0
+
+    gbg = False
+    p = 0
+    while p < len(l):
+        c = l[p]
+        if not gbg:
+            if c == '{':
+                score += len(groups) + 1
+                groups.append(p)
+            elif c == '}':
+                groups.pop()
+            elif c == '<':
+                gbg = True
+            elif c == '!':
+                p += 1
+        elif c == '>':
+            gbg = False
+        elif c == '!':
+            p += 1
+
+        p += 1
+
+    print(score)
