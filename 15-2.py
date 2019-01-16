96033384d68dd11ac8e684263c733868949e828f
karoberts <cppwriter@yahoo.com>
2019-01-16 10:11:12 -0800

    solved 15-2

---
+++ 15-2.py
@@ -0,0 +1,22 @@
+A, B = 722, 354
+#A, B = 65, 8921
+
+Af, Bf = 16807, 48271
+def gen(prev, factor):
+    return (prev * factor) % 0x7FFFFFFF
+
+a = A
+b = B
+ct = 0
+for p in range(0,5000000):
+    while True:
+        a = gen(a, Af)
+        if a % 4 == 0: break
+
+    while True:
+        b = gen(b, Bf)
+        if b % 8 == 0: break
+
+    if (a & 0xFFFF) == (b & 0xFFFF):
+        ct += 1
+print('part2', ct)
