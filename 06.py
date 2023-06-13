5ca4e57e9e9f770364745c62bac019fe623cf29d
karoberts <cppwriter@yahoo.com>
2023-06-13 11:35:32 -0700

    minor cleanup

--- 06.py
+++ 06.py
@@ -2,18 +2,12 @@ bank_data = '11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11'
 
 banks = [int(x) for x in bank_data.split(',')]
 
-def g_maxbank(banks):
-    maxi = 0
-    for i in range(1, len(banks)):
-        if banks[i] > banks[maxi]: maxi = i
-    return maxi
-
 cfgs = set()
 cycles = 0
 look_for = None
 p1 = True
 while True:
-    bank = g_maxbank(banks)
+    bank = max(range(len(banks)), key=banks.__getitem__)
     blocks = banks[bank]
     banks[bank] = 0
     bank = (bank + 1) % len(banks)
